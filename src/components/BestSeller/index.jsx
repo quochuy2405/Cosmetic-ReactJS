5bdc6ca56c66a9c549e7f5263275f820a77a5ea1
Bùi Quốc Huy <huy01682312920@gmail.com>
2021-10-27 14:15:09 +0700

    ok

--- src/components/BestSeller/index.jsx
+++ src/components/BestSeller/index.jsx
@@ -4,36 +4,54 @@ import Product from "../Product/index";
 import { useEffect } from "react";
 import { useState } from "react";
 const Listproduct = [
-  {
-    link_img: "https://imagizer.imageshack.com/img923/7854/RmX3bd.jpg",
-    title: "Title",
-    price: 70,
-  },
-  {
-    link_img: "https://imagizer.imageshack.com/img924/9743/qx2lhC.jpg",
-    title: "Title",
-    price: 70,
-  },
-  {
-    link_img: "https://imagizer.imageshack.com/img922/9882/qhXgmH.jpg",
-    title: "Title",
-    price: 70,
-  },
-  {
-    link_img: "https://imagizer.imageshack.com/img924/2301/D5Qpr4.jpg",
-    title: "Title",
-    price: 70,
-  },
-  {
-    link_img: "https://imagizer.imageshack.com/img923/8624/ssAUCe.jpg",
-    title: "Title",
-    price: 70,
-  },
-  {
-    link_img: "https://imagizer.imageshack.com/img924/9255/KbBM3Z.jpg",
-    title: "Title",
-    price: 70,
-  },
+{ link_img:"https://imagizer.imageshack.com/img922/9026/2Ard7C.jpg",
+   title:"Tiêu đề sản phầm",
+    price:70 
+},
+{ link_img:"https://imagizer.imageshack.com/img924/6235/zAXQb0.jpg",
+  title:"Tiêu đề sản phầm",
+price:70 
+},
+{ link_img:"https://imagizer.imageshack.com/img923/6209/oFXGdJ.jpg",
+  title:"Tiêu đề sản phầm",
+price:70 
+},
+{ link_img:"https://imagizer.imageshack.com/img924/2526/ElgKqg.jpg",
+  title:"Tiêu đề sản phầm",
+price:70 
+},
+{ link_img:"https://imagizer.imageshack.com/img922/9476/40c4fx.jpg",
+  title:"Tiêu đề sản phầm",
+price:70 
+},
+{ link_img:"https://imagizer.imageshack.com/img924/767/Q5vQni.jpg",
+  title:"Tiêu đề sản phầm",
+price:70 
+},
+{ link_img:"https://imagizer.imageshack.com/img922/1010/kl2FCt.jpg",
+  title:"Tiêu đề sản phầm",
+price:70 
+},
+{ link_img:"https://imagizer.imageshack.com/img923/8149/j3QrOu.jpg",
+  title:"Tiêu đề sản phầm",
+price:70 
+},
+{ link_img:"https://imagizer.imageshack.com/img924/2653/7rpjHK.jpg",
+  title:"Tiêu đề sản phầm",
+price:70 
+},
+{ link_img:"https://imagizer.imageshack.com/img922/6167/OKaNwH.jpg",
+  title:"Tiêu đề sản phầm",
+price:70 
+},
+{ link_img:"https://imagizer.imageshack.com/img924/3264/dOI89U.jpg",
+  title:"Tiêu đề sản phầm",
+price:70 
+},
+{ link_img:"https://imagizer.imageshack.com/img923/8898/5DF40f.jpg",
+  title:"Tiêu đề sản phầm",
+price:70 
+},
 ];
 function BestSeller() {
 
