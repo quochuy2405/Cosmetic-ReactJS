b5049a8ad10e4f3759510175e35b196a29dd82e7
Bùi Quốc Huy <huy01682312920@gmail.com>
2021-10-28 22:03:58 +0700

    okla

--- src/components/ProductSlider/index.jsx
+++ src/components/ProductSlider/index.jsx
@@ -27,12 +27,12 @@ const Listproduct = [
   {
     link_img: "https://imagizer.imageshack.com/img923/8624/ssAUCe.jpg",
     title: "Bain Densifique Shampoo and Conditioner",
-      price: 310,
+    price: 310,
   },
   {
     link_img: "https://imagizer.imageshack.com/img924/9255/KbBM3Z.jpg",
     title: "Keratin Smooth Color Shampoo and Conditioner",
-      price: 99,
+    price: 99,
   },
 ];
 function ProductSlider() {
@@ -47,12 +47,14 @@ function ProductSlider() {
     }
 
     if (temp < 0) {
-        temp=Listproduct.length / 2 - 1;
+      temp = Listproduct.length / 2 - 1;
     }
     if (temp > Listproduct.length / 2 - 1) {
       temp = 0;
     }
-    document.querySelector(".Products").style.transform = `translateX(-${temp * 17}%)`;
+    document.querySelector(".Products").style.transform = `translateX(-${
+      temp * 17
+    }%)`;
     SetCount(temp);
     SetFlag(true);
   };
@@ -61,46 +63,43 @@ function ProductSlider() {
       var time = setInterval(() => {
         handle_Slide(flag);
       }, 4000);
-    } 
+    }
     return () => clearInterval(time);
   }, [count]);
   return (
-      <>
-     
-    <div className="ProductSlider">
-      <div
-        onClick={() => {
-          SetFlag(true);
-          handle_Slide(true);
-        }}
-        className="btnh btn_next"
-      >
-        <i className="fas fa-chevron-circle-right"></i>
+    <>
+      <div className="ProductSlider">
+        <div
+          onClick={() => {
+            SetFlag(true);
+            handle_Slide(true);
+          }}
+          className="btnh btn_next"
+        >
+          <i className="fas fa-chevron-circle-right"></i>
+        </div>
+        <div
+          onClick={() => {
+            SetFlag(false);
+            handle_Slide(false);
+          }}
+          className="btnh btn_prev"
+        >
+          <i className="fas fa-chevron-circle-left"></i>
+        </div>
+        <div
+          className="Products"
+          style={{ width: Listproduct.length * 25 + "%" }}
+        >
+          {Listproduct.map((product, index) => (
+            <Product Product={product} key={index} />
+          ))}
+        </div>
       </div>
-      <div
-        onClick={() => {
-          SetFlag(false);
-          handle_Slide(false);
-        }}
-        className="btnh btn_prev"
-      >
-        <i className="fas fa-chevron-circle-left"></i>
-      </div>
-      <div
-        className="Products"
-        style={{ width: Listproduct.length * 25 + "%" }}
-      >
-        {Listproduct.map((product, index) => (
-           
-                   <Product Product={product} key={index} />
-         
-       
-        ))}
-      </div>
-     
-    </div>
-     <p className="View"><a href="#">View collect</a></p>
-     </>
+      <p className="View">
+        <a href="#">View collect</a>
+      </p>
+    </>
   );
 }
 
